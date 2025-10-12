20f07c77a3865837bfbabcfdb1478756ca258693
KOB-BSU <kevin.obrien@bsu.edu>
2025-10-12 06:25:46 +0000

    chore: module 6 activities

--- backend/src/__tests__/posts.test.js
+++ backend/src/__tests__/posts.test.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { describe, expect, test, beforeEach } from '@jest/globals'
+import { describe, expect, test, beforeEach, beforeAll } from '@jest/globals'
 import {
   createPost,
   listAllPosts,
@@ -10,16 +10,25 @@ import {
   deletePost,
 } from '../services/posts.js'
 import { Post } from '../db/models/post.js'
-const samplePosts = [
-  { title: 'Learning Redux', author: 'Daniel Bugl', tags: ['redux'] },
-  { title: 'Learn React Hooks', author: 'Daniel Bugl', tags: ['react'] },
-  {
-    title: 'Full-Stack React Projects',
-    author: 'Daniel Bugl',
-    tags: ['react', 'nodejs'],
-  },
-  { title: 'Guide to TypeScript' },
-]
+
+import { createUser } from '../services/users.js'
+
+let testUser = null
+let samplePosts = []
+
+beforeAll(async () => {
+  testUser = await createUser({ username: 'sample', password: 'user' })
+  samplePosts = [
+    { title: 'Learning Redux', author: testUser._id, tags: ['redux'] },
+    { title: 'Learn React Hooks', author: testUser._id, tags: ['react'] },
+    {
+      title: 'Full-Stack React Projects',
+      author: testUser._id,
+      tags: ['react', 'nodejs'],
+    },
+  ]
+})
+
 let createdSamplePosts = []
 beforeEach(async () => {
   await Post.deleteMany({})
@@ -29,6 +38,52 @@ beforeEach(async () => {
     createdSamplePosts.push(await createdPost.save())
   }
 })
+
+describe('deleting posts', () => {
+  test('should remove the post from the database', async () => {
+    const result = await deletePost(testUser._id, createdSamplePosts[0]._id)
+    expect(result.deletedCount).toEqual(1)
+    const deletedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(deletedPost).toEqual(null)
+  })
+  test('should fail if the id does not exist', async () => {
+    const result = await deletePost('000000000000000000000000')
+    expect(result.deletedCount).toEqual(0)
+  })
+})
+
+describe('updating posts', () => {
+  test('should update the specified property', async () => {
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.contents).toEqual('some content change')
+  })
+  test('should not update other properties', async () => {
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.title).toEqual('Learning Redux')
+  })
+  test('should update the updatedAt timestamp', async () => {
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
+    })
+    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
+    expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
+      createdSamplePosts[0].updatedAt.getTime(),
+    )
+  })
+  test('should fail if the id does not exist', async () => {
+    const post = await updatePost(testUser._id, '000000000000000000000000', {
+      contents: 'some content change',
+    })
+    expect(post).toEqual(null)
+  })
+})
+
 describe('getting a post', () => {
   test('should return the full post', async () => {
     const post = await getPostById(createdSamplePosts[0]._id)
@@ -39,49 +94,7 @@ describe('getting a post', () => {
     expect(post).toEqual(null)
   })
 })
-describe('updating posts', () => {
-  test('should update the specified property', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
-    })
-    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(updatedPost.author).toEqual('Test Author')
-  })
-  test('should not update other properties', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
-    })
-    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(updatedPost.title).toEqual('Learning Redux')
-  })
-  test('should update the updatedAt timestamp', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
-    })
-    const updatedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
-      createdSamplePosts[0].updatedAt.getTime(),
-    )
-  })
-  test('should fail if the id does not exist', async () => {
-    const post = await updatePost('000000000000000000000000', {
-      author: 'Test Author',
-    })
-    expect(post).toEqual(null)
-  })
-})
-describe('deleting posts', () => {
-  test('should remove the post from the database', async () => {
-    const result = await deletePost(createdSamplePosts[0]._id)
-    expect(result.deletedCount).toEqual(1)
-    const deletedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(deletedPost).toEqual(null)
-  })
-  test('should fail if the id does not exist', async () => {
-    const result = await deletePost('000000000000000000000000')
-    expect(result.deletedCount).toEqual(0)
-  })
-})
+
 describe('listing posts', () => {
   test('should return all posts', async () => {
     const posts = await listAllPosts()
@@ -109,7 +122,7 @@ describe('listing posts', () => {
     )
   })
   test('should be able to filter posts by author', async () => {
-    const posts = await listPostsByAuthor('Daniel Bugl')
+    const posts = await listPostsByAuthor(testUser.username)
     expect(posts.length).toBe(3)
   })
   test('should be able to filter posts by tag', async () => {
@@ -121,11 +134,10 @@ describe('creating posts', () => {
   test('with all parameters should succeed', async () => {
     const post = {
       title: 'Hello Mongoose!',
-      author: 'Huseyin Ergin',
       contents: 'This post is stored in a MongoDB database using Mongoose.',
       tags: ['mongoose', 'mongodb'],
     }
-    const createdPost = await createPost(post)
+    const createdPost = await createPost(testUser._id, post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
     const foundPost = await Post.findById(createdPost._id)
     expect(foundPost).toEqual(expect.objectContaining(post))
@@ -134,12 +146,11 @@ describe('creating posts', () => {
   })
   test('without title should fail', async () => {
     const post = {
-      author: 'Huseyin Ergin',
       contents: 'Post with no title',
       tags: ['empty'],
     }
     try {
-      await createPost(post)
+      await createPost(testUser._id, post)
     } catch (err) {
       expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
       expect(err.message).toContain('`title` is required')
@@ -149,7 +160,7 @@ describe('creating posts', () => {
     const post = {
       title: 'Only a title',
     }
-    const createdPost = await createPost(post)
+    const createdPost = await createPost(testUser._id, post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
   })
 })
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,15 @@
-import { PostList } from './components/PostList.jsx'
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
-import { useQuery } from '@tanstack/react-query'
-import { getPosts } from './api/posts.js'
+import { PostList } from '../components/PostList.jsx'
+import { CreatePost } from '../components/CreatePost.jsx'
+import { PostFilter } from '../components/PostFilter.jsx'
+import { PostSorting } from '../components/PostSorting.jsx'
+
+import { Header } from '../components/Header.jsx'
+
 import { useState } from 'react'
+
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from '../api/posts.js'
+
 export function Blog() {
   const [author, setAuthor] = useState('')
   const [sortBy, setSortBy] = useState('createdAt')
@@ -16,7 +21,9 @@ export function Blog() {
   const posts = postsQuery.data ?? []
   return (
     <div style={{ padding: 8 }}>
-      <h1>Welcome to my assignment blog.</h1>
+      <Header />
+      <br />
+      <hr />
       <CreatePost />
       <br />
       <hr />
